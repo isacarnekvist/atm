c25b0784426296788dcc43a368b9ecb252d41f3f
styrarN <felix.engstrom@gmail.com>
2014-12-02 21:55:09 +0100

    and again

--- server.go
+++ server.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "bufio"
-    "ATM/Graph.zip/lib"
+    "bytesmaker"
     "errors"
     "fmt"
     "net"
@@ -35,6 +35,7 @@ const (
 
 /* Data about user stored in this struct */
 type User struct {
+    id          int
     password    int64 
     balance     int64
     temp_code   []int       /* A list of single use numbers to use when withdrawing */
@@ -103,14 +104,31 @@ func scan_uint() int {
  * go routines to handle each one.
  */
 func start_listening() {
-    ln, _ := net.Listen("tcp", ":8080")
+
+    var port string
+    if len(os.Args) == 2 {
+        port = os.Args[1]
+    } else {
+        port = "8080"
+    }
+
+    ln, err := net.Listen("tcp", ":" + port)
+    if err != nil {
+            fmt.Printf("Couldn't start server \n")
+            panic(err)
+        } else {
+            fmt.Printf("Server started successfully \n")
+        }
+
+    /* Start connection with all requests */
     for {
         c, _ := ln.Accept()
         go handleConnection(c)
     }
 }
 
-/* This handles that a connections goes between states
+/* 
+ * This handles that a connection goes between states
  * in a correct manner and returns if connection was
  * lost
  */
@@ -123,8 +141,11 @@ func handleConnection(c net.Conn) {
         user, err2 := state_login(c)
         if err2 != nil { break }
 
-        err = state_user(user, c)
-        if err != nil { break }
+        /* Do no go to user state if client requested logout */
+        if user.id != 0 {   
+            err = state_user(user, c)
+            if err != nil { break }
+        }
     }
     c.Close()
     print("Connection closed\n")
@@ -134,68 +155,72 @@ func handleConnection(c net.Conn) {
 func state_updates(c net.Conn) error {
     fmt.Printf("Entered UPDATES state \n")
     d := bytesmaker.Bytes(byte(server_no_updates), byte(0), int64(0))
-    fmt.Printf("Sent: %d \n", d)
     _, err := c.Write(d)
 
     return err
 }
 
+
+
 /*
  * Listens for login requests from client
- * Returns a valid user or error
+ * Returns: 
+ *  - A confirmed user and nil if successful
+ *  - Empty User{} and error if connection lost
+ *  - Empty User{} and nil if logout was requested
  */
 func state_login(c net.Conn) (User, error) {
     
-    var user User
     fmt.Printf("Entering LOGIN state \n")
 
-    /* Start with user id verification */
+    /* Initialize local variables */
+    valid_id_sent := false
+    user          := User{}
+
     for {
-        op, id, err := read_and_decode(c)
+        op, val, err := read_and_decode(c)
         if err != nil { return User{}, err }                    /* Connection was probably closed */
-        if op != login_number {                                 /* Client sent unexpected op-code */
-            send_ten( server_error, 0, c)                       /* Respond with error */
-            fmt.Printf("Client sent unexpected op code \n")
-            return User{}, errors.New("Unexpected op code")     /* Close connection */
-        }
-        usertemp, user_exists := user_db[id]
-        user = usertemp
 
-        if user_exists {
-            fmt.Printf("Client sent valid id \n")
-            send_ten( server_accept, 0, c )
-            break                                               /* Go on with passcode check */
-        } else {
-            fmt.Printf("Client sent non-valid id \n")
-            send_ten( server_decline, 0, c )
-        }
+        switch op {
+            
+        case login_number:
+            
+            user_temp, user_exists := user_db[val]
+            if user_exists {
+                send_accept(c)
+                user = user_temp;
+                valid_id_sent = true;
+            } else {
+                send_decline(c)
+            }
 
+
+        case login_pwd:
+
+            if valid_id_sent { 
+                if user.password == val {
+                    send_accept(c)
+                    return user, nil
+                } else {
+                    send_decline(c)
+                }
+            } else {
+                return User{}, errors.New("Unexpected password before successful login")    
+            }
+
+
+        case user_logout:
+
+            return user, nil
+
+        default:
+
+            return User{}, errors.New("Unexpected op code in login")
+
+        }
     }
-
-    /* Password verification */
-    for {
-        op, pass, err := read_and_decode(c)
-        if err != nil { return User{}, err }                    /* Connection was probably closed */
-        if op != login_pwd {                                    /* Client sent unexpected op-code */
-            send_ten( server_error, 0, c )                      /* Respond with error */
-            fmt.Printf("Client sent unexpected op code \n")
-            return User{}, errors.New("Unexpected op code")     /* Close connection */
-        }
-
-        if user.password == pass {
-            fmt.Printf("Client sent valid password \n")
-            send_ten( server_accept, 0, c )
-            break                                               /* Leave LOGIN state */
-        } else {
-            fmt.Printf("Client sent non-valid password \n")
-            send_ten( server_decline, 0, c )
-        }
-    }
-
-    fmt.Printf("Leaving LOGIN state \n")
-
-    return user, nil
 }
+
 
 /*
  * Listens for user requests
@@ -218,7 +243,8 @@ func state_user(user User, c net.Conn) error {
             /* Let's accept negative balance! More income for the bank! */
             if code == int64(valid_single_use_code) {
                 user.balance -= amount         
-                user.temp_index++   
+                user.temp_index++
+                user_db[int64(user.id)] = user                  /* Changed user-state needs to be written back */
                 send_ten( server_accept, 0, c )
             } else {
                 send_ten( server_decline, 0, c )
@@ -245,18 +271,26 @@ func init_user_db() {
 
     /* Add some users */
     user_db[86] = User {
+        id:         86,
         password:   1234,
         balance:    1000,
         temp_code:  odd_ints(),
         temp_index: 0,
     }
     user_db[85] = User {
+        id:         85,
         password:   1111,
         balance:    2000,
         temp_code:  odd_ints(),
         temp_index: 0,
     }
 }
+
+/***************************************/
+/*                                     */
+/* A lot of convenience methods follow */
+/*                                     */
+/***************************************/
 
 /*
  * Reads and returns op-code, value, error
@@ -278,6 +312,13 @@ func send_ten(op int, val int64, c net.Conn) {
     c.Write(data)
 }
 
+func send_decline(c net.Conn) {
+    send_ten( server_decline, 0, c )
+}
+
+func send_accept(c net.Conn) {
+    send_ten( server_accept, 0, c )
+}
 
 /*
  * Returns a slice of all odd numbers 1 - 99
